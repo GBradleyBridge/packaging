9af50a51eb17cfcac244f1ae0e8ac3f6cea767d8
Glen Haber <glen.haber@thebridgecorp.com>
2022-10-05 11:48:43 -0400

    Fix importability of dummy dag 2

--- src/deployed_dags/two.py
+++ src/deployed_dags/two.py
@@ -7,5 +7,5 @@ with DAG(
         dag_id='dummy-dag-2',
         start_date=datetime(2022, 10, 5),
         schedule_interval=None,
-):
+) as dag:
     DummyOperator(task_id='one') >> [DummyOperator(task_id='two'), DummyOperator(task_id='three')] >> DummyOperator(task_id='four')
