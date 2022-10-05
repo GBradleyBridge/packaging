7b7b46a5b805c0370bdaf3b6ea4d66fd4a8b99aa
Glen Haber <glen.haber@thebridgecorp.com>
2022-10-05 09:55:57 -0400

    Create some dummy DAG files to deploy

---
+++ src/deployed_dags/one.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+
+with DAG(
+        dag_id='dummy-dag-1',
+        start_date=datetime(2022, 10, 5),
+        schedule_interval=None
+) as dag:
+    DummyOperator(task_id='one') >> DummyOperator(task_id='two') >> DummyOperator(task_id='three')
---
+++ src/deployed_dags/two.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+
+with DAG(
+        dag_id='dummy-dag-2',
+        start_date=datetime(2022, 10, 5),
+        schedule_interval=None,
+):
+    DummyOperator(task_id='one') >> [DummyOperator(task_id='two'), DummyOperator(task_id='three')] >> DummyOperator(task_id='four')
